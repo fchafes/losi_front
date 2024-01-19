7f6c06c6e53c29cc9854506b3f4976949a8f5c02
valentinalegre <valuualegre15@icloud.com>
2024-01-19 20:22:26 -0300

    header ejemplo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import "./Header.css";
 const Header = () => {
     return (
         <header className="header">
-        <img className="header-img" src="/public/cap.png" alt="" />
+        <img className="header-img" src="/public/header.jpg" alt="" />
       </header>
         );
 };
