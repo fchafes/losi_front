8194990b01169b36e81968b2f4de78dc87926f9f
valentinalegre <valuualegre15@icloud.com>
2024-01-23 19:19:31 -0300

    arreglo la ruta de la imagen

--- src/components/Parallax.jsx
+++ src/components/Parallax.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Parallax.css'; 
+import parallaxImage from '/public/picture3.webp';
+
 
 const Parallax = () => {
   return (
     <div className="container">
-      <div className="parallax">
+      <div className="parallax" style={{ backgroundImage: `url(${parallaxImage})` }}>
           <div className="left"></div>
         </div>
       </div>
