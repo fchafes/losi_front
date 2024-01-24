3847d19313060e17d347399d8851769406f9df20
valentinalegre <valuualegre15@icloud.com>
2024-01-24 20:56:34 -0300

    arreglo el espaciado y cambio el boton de about us

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,24 +7,27 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 const Header = () => {
     return (
         <div className="header">
-            <div className="carousel-wrapper">
+            <div className="carousel-image">
                 <Carousel autoPlay infiniteLoop interval={3000} showThumbs={false}>
                     <div className="carousel-slide">
-                       <video className="carousel-video" autoPlay loop muted style={{ width: "100%" }}>
+                       <video className="carousel-image" autoPlay loop muted style={{ width: "100%" }}>
                             <source src={"prueba.mp4"} type="video/mp4" />
-                            </video>                      
+                            </video>                  
+                            <div className="carousel-image-overlay">
+                            <button className="discover-button">About Us</button>
+                        </div>    
                     </div>
                     
                     <div className="carousel-slide">
                         <img className="carousel-image" src={"P2144034.jpg"} alt="Slide 2" />
                         <div className="carousel-image-overlay">
-                            <button className="discover-button">Discover</button>
+                            <button className="discover-button">About Us</button>
                         </div>
                     </div>
                     <div className="carousel-slide">
                         <img className="carousel-image" src={"/4remeras.jpg"} alt="Slide 3" />
                         <div className="carousel-image-overlay">
-                            <button className="discover-button">Discover</button>
+                            <button className="discover-button">About Us</button>
                         </div>
                     </div>
                 </Carousel>
