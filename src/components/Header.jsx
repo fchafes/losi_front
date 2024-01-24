d7eda55a3a6cc52ac8d328763935dbfe98273164
valentinalegre <valuualegre15@icloud.com>
2024-01-23 22:53:17 -0300

    Cambios

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,6 @@ import "./Header.css";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-import foto2 from "/public/pic12.jpg";
-import foto3 from "/public/pic1.jpg";
-import foto4 from "/public/pic4.jpg";
 
 const Header = () => {
     return (
@@ -13,19 +10,19 @@ const Header = () => {
             <div className="carousel-wrapper">
                 <Carousel autoPlay infiniteLoop interval={3000} showThumbs={false}>
                     <div className="carousel-slide">
-                        <img className="carousel-image" src={"/Bananaslide.JPG"} alt="Slide 1" />
+                        <img className="carousel-image" src={"letter2.png"} alt="Slide 1" />
                         <div className="carousel-image-overlay">
                             <button className="discover-button">Discover</button>
                         </div>
                     </div>
                     <div className="carousel-slide">
-                        <img className="carousel-image" src={"/Camisa.JPG"} alt="Slide 2" />
+                        <img className="carousel-image" src={"P2144034.jpg"} alt="Slide 2" />
                         <div className="carousel-image-overlay">
                             <button className="discover-button">Discover</button>
                         </div>
                     </div>
                     <div className="carousel-slide">
-                        <img className="carousel-image" src={"/ollie.JPG"} alt="Slide 3" />
+                        <img className="carousel-image" src={"/mbn.png"} alt="Slide 3" />
                         <div className="carousel-image-overlay">
                             <button className="discover-button">Discover</button>
                         </div>
