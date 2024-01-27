fb688f07e600970a4c5557d30b2c4df6d59bc85d
ThomiHughes <thomih44@gmail.com>
2024-01-27 03:27:53 -0300

    click en productos de featured con link

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./Featured.css";
 import FeaturedItem from "./FeaturedItem";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Featured = () => {
   const [products, setProducts] = useState([]);
@@ -29,12 +30,13 @@ const Featured = () => {
       <h2 className="featured-title">FEATURED PRODUCTS</h2>
       <div className="featured-container">
         {products.map((product) => (
-          <FeaturedItem
-            key={product.id}
-            imageSrc={product.photo}
-            categoryLink={`/product/${product.id}`}
-            description={`${product.name}\n$${product.price}`}
-          />
+          <Link to={`/product/${product.id}`}>
+            <FeaturedItem
+              key={product.id}
+              imageSrc={product.photo}
+              description={`${product.name}\n$${product.price}`}
+            />
+          </Link>
         ))}
       </div>
     </div>
