14f88c89099e4634dcf658153368986c8e4a2415
juaniboni <juanignacioboni@gmail.com>
2024-01-31 21:12:01 -0300

    Imports

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
-import { configureStore } from '@reduxjs/toolkit'
-import { Provider } from 'react-redux'
-import { persistStore } from 'redux-persist';
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
 import persistedCartReducer from "./redux/cartReducer.js";
-import { PersistGate } from 'redux-persist/integration/react';
+import { PersistGate } from "redux-persist/integration/react";
 
 const store = configureStore({
   reducer: { cart: persistedCartReducer },
