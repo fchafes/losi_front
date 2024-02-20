52df3ad383ea74b6887def214036afe2e6f0193b
ThomiHughes <thomih44@gmail.com>
2024-02-20 01:03:56 -0500

    checkout mejoras

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -15,12 +15,12 @@ const Checkout = () => {
   const user = useSelector((state) => state.customer.user);
   const dispatch = useDispatch();
   const [paymentMethod, setPaymentMethod] = useState("Mercado Pago");
-  const [shippingAddress, setShippingAddress] = useState(user.customer.address);
+  const [shippingAddress, setShippingAddress] = useState(user ? user.customer.address : '');
   const [showInfo, setShowInfo] = useState(true);
-  
+
   const handleShowInfo = () => {
     setShowInfo(false);
-  }
+  };
 
   const calculateSubtotal = () => {
     let subtotal = 0;
@@ -39,7 +39,7 @@ const Checkout = () => {
 
   const handleRemoveAllItems = () => {
     dispatch(removeAllItems());
-  }
+  };
 
   const handleDecrementQuantity = (id) => {
     dispatch(decrementQuantity(id));
@@ -53,6 +53,7 @@ const Checkout = () => {
   };
 
   const handleCheckout = async () => {
+    
     try {
       const response = await axios.post("http://localhost:3000/orders", {
         customerId: user.customer.id, // Assuming you have the customer ID in your user object
@@ -76,69 +77,79 @@ const Checkout = () => {
 
   return (
     <div className="checkout-page-container">
-    {showInfo ? (
-      <>
-        <div className="checkout-items">
-          <h2>Cart Items</h2>
-          {cartItems.map((item) => (
-            <div key={item.id}>
-              <div className="checkout-item-container ">
-                <div className="checkout-item-text">
-                  <p>{item.name}</p>
-                  <p>Size: {item.selectedSize}</p>
-                  <p>Quantity:{item.quantity}</p>
-                  <div className="checkout-article-body-options-counter-action">
-                    <p
-                      onClick={() =>
-                        handleRemoveFromCart(item.id, item.selectedSize)
-                      }
-                    >
-                      Remove
-                    </p>
+      {showInfo ? (
+        <>
+          <div className="checkout-items">
+            <h2>Cart Items</h2>
+            {cartItems.map((item) => (
+              <div key={item.id}>
+                <div className="checkout-item-container ">
+                  <div className="checkout-item-text">
+                    <p>{item.name}</p>
+                    <p>Size: {item.selectedSize}</p>
+                    <p>Quantity:{item.quantity}</p>
+                    <div className="checkout-article-body-options-counter-action">
+                      <p
+                        onClick={() =>
+                          handleRemoveFromCart(item.id, item.selectedSize)
+                        }
+                      >
+                        Remove
+                      </p>
+                    </div>
                   </div>
+                  <img
+                    src={item.photo}
+                    className="checkout-item-image"
+                    alt=""
+                  />
                 </div>
-                <img src={item.photo} className="checkout-item-image" alt="" />
               </div>
+            ))}
+          </div>
+          <div className="customer-info">
+            <div className="payment-info">
+              <h2>Payment Information</h2>
+              <form>
+                <label>
+                  Payment Method:
+                  <select
+                    value={paymentMethod}
+                    onChange={handlePaymentMethodChange}
+                  >
+                    <option value="Mercado Pago">Mercado Pago</option>
+                    <option value="Paypal">Paypal</option>
+                    <option value="Credit Card">Credit Card</option>
+                  </select>
+                </label>
+                <label>
+                  Shipping Address:
+                  <input
+                    type="text"
+                    value={shippingAddress}
+                    onChange={handleShippingAddressChange}
+                  />
+                </label>
+                <div className="cart-subtotal-checkout">
+                  <p>Subtotal ---&gt; ${calculateSubtotal()}</p>
+                </div>
+                <button type="button" onClick={handleCheckout}>
+                  Checkout
+                </button>
+              </form>
             </div>
-          ))}
-          <div className="cart-subtotal-checkout">
-            <p>Subtotal ---&gt; ${calculateSubtotal()}</p>
           </div>
+        </>
+      ) : (
+        <div className="order-completed">
+          <h3>Order completed!</h3>
+          <p>
+            You will received an email with the information of the order and
+            your order ID.
+          </p>
         </div>
-        <div className="customer-info">
-          <div className="payment-info">
-            <h2>Payment Information</h2>
-            <form>
-              <label>
-                Payment Method:
-                <select value={paymentMethod} onChange={handlePaymentMethodChange}>
-                  <option value="Mercado Pago">Mercado Pago</option>
-                  <option value="Paypal">Paypal</option>
-                  <option value="Credit Card">Credit Card</option>
-                </select>
-              </label>
-              <label>
-                Shipping Address:
-                <input
-                  type="text"
-                  value={shippingAddress}
-                  onChange={handleShippingAddressChange}
-                />
-              </label>
-              <button type="button" onClick={handleCheckout}>
-                Checkout
-              </button>
-            </form>
-          </div>
-        </div>
-      </>
-    ) : (
-      <div className="order-completed">
-      <h3>Order completed!</h3>
-      <p>You will received an email with the information of the order and your order ID.</p>
-      </div>
-    )}
-  </div>
+      )}
+    </div>
   );
 };
 
