1153232b2b905a92b8b263ef752abb50767a2c36
valentinalegre <valuualegre15@icloud.com>
2024-02-26 18:01:50 -0300

    cambio color cursor parallax

--- src/components/Parallax.jsx
+++ src/components/Parallax.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Parallax.css'; 
 import parallaxImage from '/public/picture3.webp';
 
-
 const Parallax = () => {
+  const [isColored, setIsColored] = useState(false);
+
+  const toggleColor = () => {
+    setIsColored(!isColored);
+  };
+
   return (
     <div className="container">
-      <div className="parallax" style={{ backgroundImage: `url(${parallaxImage})` }}>
-          <div className="left"></div>
-        </div>
+      <div
+        className={`parallax ${isColored ? '' : 'grayscale'}`} // Invertimos las clases
+        onMouseEnter={toggleColor}
+        onMouseLeave={toggleColor}
+        style={{ backgroundImage: `url(${parallaxImage})` }}
+      >
+        <div className="left"></div>
       </div>
+    </div>
   );
 };
 
