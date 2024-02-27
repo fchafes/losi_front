185f198f7988dabb5fb2c3fb4ed7b0f4a2a8857d
valentinalegre <valuualegre15@icloud.com>
2024-02-27 14:42:35 -0300

    responsive en productos

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import axios from "axios";
-import React, { useEffect } from "react";
->>>>>>> parent of d209295 (arreglos)
+import { useEffect } from "react"; 
 import { useLocation } from "react-router-dom";
+
 
 function Feedback() {
   const location = useLocation();
