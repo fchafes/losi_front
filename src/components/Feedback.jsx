a6d8952d4d7eca64fd5fb885fed1631f171cb779
ThomiHughes <thomih44@gmail.com>
2024-02-27 13:24:08 -0300

    Revert "arreglos"
    This reverts commit d20929514905d2098b67715ba89a275358d67390.

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,10 +1,5 @@
 import React from "react";
 import axios from "axios";
-<<<<<<< HEAD
-import React, { useEffect, useState } from "react";
-=======
-import { useLocation } from "react-router-dom";
-
 import React, { useEffect } from "react";
 >>>>>>> parent of d209295 (arreglos)
 import { useLocation } from "react-router-dom";
