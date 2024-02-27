5ef69ac9dc65f354888e9073131063eabe1b9bc6
ThomiHughes <thomih44@gmail.com>
2024-02-27 03:50:15 -0300

    .

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -19,26 +19,7 @@ function Feedback() {
     fetchData();
   }, []);
 
-  return (
-    <div>
-      <h1>Feedback Page</h1>
-      {orderData ? (
-        <div>
-          <h2>Order ID: {orderData.id}</h2>
-          <h3>Total Amount: {orderData.total_amount}</h3>
-          <ul>
-            {orderData.items.map((item, index) => (
-              <li key={index}>
-                {item.title} - Quantity: {item.quantity} - Unit Price: {item.unit_price}
-              </li>
-            ))}
-          </ul>
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
-  );
+  
 }
 
 export default Feedback;
